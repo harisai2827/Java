d5db5c3d71db618ea127b5ba651dec9f263b8c89
Hari <harialumolu.gmail.com>
2026-07-30 15:38:21 +0530

    Added day 3

---
+++ day 3/ass22.java
@@ -0,0 +1,26 @@
+public class ass22 {
+    public static void main(String[] args) {
+
+        int[] arr = {2, 3, 2, 5, 3, 2};
+
+        boolean[] visited = new boolean[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (visited[i])
+                continue;
+
+            int count = 1;
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] == arr[j]) {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+
+            System.out.println(arr[i] + " occurs " + count + " times");
+        }
+    }
+}
