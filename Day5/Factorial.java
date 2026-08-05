b80f2f471ab3272cd12893d76aa40cf356a3a51a
Hari Alumolu <harialumolu@gmail.com>
2026-08-05 14:44:59 +0530

    Create Factorial.java

---
+++ Day5/Factorial.java
@@ -0,0 +1,11 @@
+class Solution {
+    static long factorial(int n) {
+        long fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+
+        return fact;
+    }
+}
