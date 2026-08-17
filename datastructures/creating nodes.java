de0211ea9c47d1e9ef8dc2a61671eba0cf1206d1
Hari Alumolu <harialumolu@gmail.com>
2026-08-17 15:04:04 +0530

    Create creating nodes.java

---
+++ datastructures/creating nodes.java
@@ -0,0 +1,19 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Node n1 = new Node(10);
+
+        System.out.println(n1.data);
+        System.out.println(n1.next);
+    }
+}
