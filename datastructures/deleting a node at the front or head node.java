e620d21b1c1d9e1ef6b6491bbf2c53051872e26a
Hari Alumolu <harialumolu@gmail.com>
2026-08-17 15:16:51 +0530

    Create deleting a node at the front or head node.java

---
+++ datastructures/deleting a node at the front or head node.java
@@ -0,0 +1,46 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Main {
+
+    static Node deleteAtFront(Node head) {
+
+        if (head == null) {
+            return null;
+        }
+
+        head = head.next;
+
+        return head;
+    }
+
+    static void printList(Node head) {
+        Node curr = head;
+
+        while (curr != null) {
+            System.out.print(curr.data + " -> ");
+            curr = curr.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+
+        head = deleteAtFront(head);
+
+        printList(head);
+    }
+}
